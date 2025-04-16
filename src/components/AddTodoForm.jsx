242e34e3ffcf79626f53760cbdcf808107049826
Adel <adeleslami0077@yahoo.com>
2025-04-16 13:22:52 +0330

    create AddTodoForm component

---
+++ src/components/AddTodoForm.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function AddTodoForm() {
+    const [value,setValue] = useState("")
+  return (
+    <form className="flex flex-col justify-center mt-3 mb-4 px-4">
+      <label htmlFor="name" className="mb-1">
+        Name
+      </label>
+      <input
+        autoComplete="off"
+        id="name"
+        type="text"
+        className="border rounded-md px-4 py-2 border-gray-400 focus:outline-none text-gray-700 "
+        placeholder="Add todo ..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="p-2 bg-blue-600 rounded text-white cursor-pointer mt-4 ">submit</button>
+
+    </form>
+  );
+}
+
+export default AddTodoForm;
